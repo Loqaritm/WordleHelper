9c755790b499c861bc44652cbbb2471341e57f00
Marcel Śliwiński <marcel.sliwinski@gmail.com>
2022-05-29 23:23:44 +0200

    Added the option to input the yellow characters into the pattern

--- WordleHelper.py
+++ WordleHelper.py
@@ -1,12 +1,13 @@
 import argparse
 import re
 
-#This is basically a simple wrapper around a simple regex. But I wanted to play around with the argparse lib
+# This is basically a simple wrapper around a simple regex. But I wanted to play around with the argparse lib
 
 parser = argparse.ArgumentParser(description='Returns a list of words matching the input of known chars (green). Use _ to denote unknowns.')
-parser.add_argument('formula', type=str, help='The formula to use. Denote unknowns known characters with themselves, unknown with _, e.g. cr_ne')
+parser.add_argument('pattern', type=str, help='The pattern to use. Denote green characters with themselves, yellow with {char}, unknown with _,\n\
+for example: C_{R}NE will return CRANE')
 parser.add_argument('-l', '--limit', type=int, required=False, help='Limit the number of output lines')
-parser.add_argument('-c', '--contains', type=str, required=False, help='All characters that without known position (yellow), e.g. pen')
+parser.add_argument('-c', '--contains', type=str, required=False, help='All characters that without known position (yellow), e.g. --contains pen')
 
 args = parser.parse_args()
 
@@ -14,17 +15,24 @@ with open('valid-wordle-words.txt') as file:
     lines = file.readlines()
     lines = [line.strip().lower() for line in lines]
 
-    regexFormula = args.formula.lower().replace('_', '.')
+    inputPattern = args.pattern.lower()
 
-    r = re.compile(regexFormula)
+    # Find all the yellow characters
+    yellowCharactersInPattern = re.findall(r'.*\{(.)\}.*', inputPattern)
+
+    # Replace yellow characters in pattern with _ (meaning any characters)
+    inputPattern = re.sub(r'\{.\}', '_', inputPattern)
+    
+    regexPattern = inputPattern.replace('_', '.')
+    r = re.compile(regexPattern)
     matchingList = list(filter(r.match, lines))
 
     if (args.limit is not None):
         matchingList = matchingList[:args.limit]
 
-    if (args.contains is not None):
-        knownChars = list(args.contains)
-        matchingList = [x for x in matchingList if any(c in knownChars for c in x)]
+    if (args.contains is not None or yellowCharactersInPattern):
+        knownChars = list(args.contains) + yellowCharactersInPattern
+        matchingList = [x for x in matchingList if all(c in x for c in knownChars)]
 
     for match in matchingList:
         print(match)
